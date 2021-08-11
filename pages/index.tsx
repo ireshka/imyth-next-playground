68fe9d99e294c38cd1b843c7286cbf68308177b7
ireshka <ireshka@users.noreply.github.com>
2021-08-11 18:04:17 +0200

    refactor: rewrite Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { ReactNode } from "react";
-import { TextLink } from "../components/shared/TextLink/TextLink.component";
 import { docs, GetDocsMetadataSortedResult } from "../lib/docs";
 import { DocsList } from "../components/shared/DocsList/DocsList.component";
 import { DocsListElement } from "../components/shared/DocsListElement/DocsListElement.component";
 import { SITE_TITLE } from "../constants/siteInfo.constants";
 import { Heading } from "../components/shared/Heading/Heading.component";
-import styles from "../styles/pages/Home.module.css";
+import { Introduction } from "../components/HomePage/Introduction/Introduction.component";
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const allDocs = await docs.getDocsMetadataSorted({
@@ -32,14 +30,7 @@ export default function Home({ allDocs }: Props): ReactNode {
       <Head>
         <title>{SITE_TITLE}</title>
       </Head>
-      <section className={styles.introduction}>
-        It is a <code>next.js</code> playground about mythological creatures. If
-        you want to know more, click and check{" "}
-        <Link href="/about" passHref>
-          <TextLink underline>About Page</TextLink>
-        </Link>
-        .
-      </section>
+      <Introduction />
       <section>
         <Heading>Posts List</Heading>
         <DocsList>
