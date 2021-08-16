261ba720091b2f952f7ba310abefa4d7d376a30d
ireshka <ireshka@users.noreply.github.com>
2021-08-16 20:11:02 +0200

    chore(config): extend basic config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,130 +8,114 @@ module.exports = {
     /**
      * @info General
      */
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:prettier/recommended",
-    "plugin:promise/recommended",
-    "plugin:unicorn/recommended",
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
     /**
      * @info React
      */
-    "airbnb",
-    "airbnb/hooks",
+    'airbnb',
+    'airbnb/hooks',
     /**
      * @info Typescript
      */
-    "airbnb-typescript",
-    "plugin:@typescript-eslint/recommended",
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
     /**
      * @info Prettier
      */
-    "prettier",
+    'prettier',
     /**
      * @info Next
      */
-    "next",
-    "next/core-web-vitals",
+    'next',
+    'next/core-web-vitals',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2020,
-    sourceType: "module",
-    project: "./tsconfig.json",
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: [
     /**
      * @info General eslint plugins
      */
-    "prettier",
-    "promise",
-    "unicorn",
+    'prettier',
+    'promise',
+    'unicorn',
     /**
      * @info Typescript
      */
-    "@typescript-eslint",
+    '@typescript-eslint',
   ],
   rules: {
     /**
      * @info eslint-plugin-import
      */
-    "import/prefer-default-export": "off",
+    'import/prefer-default-export': 'off',
     /**
      * @info unicorn rules
      */
-    "unicorn/filename-case": "off",
-    "unicorn/import-index": "off",
-    "unicorn/import-style": "off",
-    "unicorn/no-null": "off",
-    "unicorn/no-reduce": "off",
-    "unicorn/numeric-separators-style": "off",
-    "unicorn/prefer-ternary": "off",
-    "unicorn/prefer-node-protocol": "off",
-    "unicorn/prefer-type-error": "off",
-    "unicorn/prevent-abbreviations": [
-      "off",
-      // {
-      //   checkFilenames: false,
-      //   checkProperties: true,
-      //   replacements: {
-      //     props: {
-      //       properties: false,
-      //     },
-      //     ref: {
-      //       reference: false,
-      //     },
-      //     docs: false,
-      //     doc: false,
-      //   },
-      // },
-    ],
+    'unicorn/filename-case': 'off',
+    'unicorn/import-index': 'off',
+    'unicorn/import-style': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/no-reduce': 'off',
+    'unicorn/numeric-separators-style': 'off',
+    'unicorn/prefer-ternary': 'off',
+    'unicorn/prefer-node-protocol': 'off',
+    'unicorn/prefer-type-error': 'off',
+    'unicorn/prevent-abbreviations': ['off'],
     /**
      * @info react rules
      */
-    "react/no-danger": "off",
+    'react/no-danger': 'off',
     /**
      * @info for React 17+
      */
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-props-no-spreading": "off",
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
     /**
      * @info quick fixes
      */
-    "no-console": "off",
-    "react/prop-types": "off",
-    "jsx-a11y/anchor-is-valid": [
-      "error",
+    'no-console': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
       {
-        components: ["Link"],
-        specialLink: ["hrefLeft", "hrefRight"],
-        aspects: ["invalidHref", "preferButton"],
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
       },
     ],
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-        paths: ["src"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['src'],
       },
     },
     jest: {
-      version: "detect",
+      version: 'detect',
     },
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   overrides: [
     {
-      files: [".eslintrc.js", "*.config.js"],
+      files: ['.eslintrc.js', '*.config.js'],
       rules: {
-        "unicorn/prevent-abbreviations": "off",
+        'unicorn/prevent-abbreviations': 'off',
       },
     },
   ],
